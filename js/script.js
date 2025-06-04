2646ba3c9204895deadc461d137e13f0d6a90cd7
Arley <abenagil456@gmail.com>
2025-06-03 20:57:01 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const icono = document.querySelector('div img');
+  if (icono) {
+    icono.src = 'images/logo.svg';
+  }
+
+  const links = document.querySelectorAll('.menu-header a')
+  if (links.length > 0) {
+    links[0].textContent = 'Features'
+    links[0].href = '#'
+
+    links[1].textContent = 'Team'
+    links[1].href = '#'
+
+    links[2].textContent = 'Sign In'
+    links[2].href = '#'
+  }
+
+  const imagen = document.querySelector('#content img');
+  if (imagen) {
+    imagen.src = 'images/illustration-intro.png'
+  }
+
+  let titulo = document.querySelector('#content h1');
+  titulo.innerHTML = 'All your files in one secure location,</br> accessible anywhere.'
+
+  let description = document.querySelector('#content p');
+  description.innerHTML = 'Fylo stores all your most important files in one secure location.</br> Access them wherever you need, share and collaborate with</br> friends family, and co-workers.'
+
+  const boton = document.getElementById('start');
+  boton.value = 'get started'
+  boton.textContent = 'Get Started'
+
+});
