ed91da9e11886cf2d2129b10d57e769257b2580d
Arley <abenagil456@gmail.com>
2025-06-07 15:43:35 -0500

    Upadate css, html, script

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const links = document.querySelectorAll('.menu-header a')
   if (links.length > 0) {
     links[0].textContent = 'Features'
-    links[0].href = '#'
+    links[0].href = '#feature'
 
     links[1].textContent = 'Team'
     links[1].href = '#'
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
   boton.textContent = 'Get Started'
   boton.href ="#"
 
-  const images = document.querySelectorAll('.features img');
+  const images = document.querySelectorAll('.features-content img');
   const imageSources = [
     "images/icon-access-anywhere.svg",
     "images/icon-security.svg",
@@ -44,4 +44,34 @@ document.addEventListener('DOMContentLoaded', () => {
       image.src = imageSources[index];
     }
   })
+
+  function asignarValoresElements(elements, texts) {
+    elements.forEach((element, index) => {
+      if (index < texts.length) {
+        element.innerHTML = texts[index];
+      }
+    })
+  }
+
+  let titleItem = document.querySelectorAll('.features-content strong');
+  let paragraphItem = document.querySelectorAll('.features-content p')
+  
+  let titles = [
+    "Access your files, anywhere",
+    "Security you can trust",
+    "Real-time collaboration",
+    "Store any type of file"
+  ]
+
+  let paragraph = [
+    "The ability to use a smartphone, tablet, or computer<br> to access your account means your files follow you<br> everywhere.",
+    "2-factor authentication and user-controlled encryption are<br> just a couple of the security features we allow to help<br> secure your files.",
+    "Securely share files and folders with friends, family and<br> colleagues for live collaboration. No email attachments<br> required.",
+    "Whether you're sharing holidays photos or work<br> documents, Fylo has you covered allowing for all file<br> types to be securely stored and shared."
+  ]
+
+  asignarValoresElements(titleItem, titles)
+  asignarValoresElements(paragraphItem, paragraph)
+
+
 });
