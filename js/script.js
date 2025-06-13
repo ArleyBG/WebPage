b6023a34e6601d0f93e86db50ea3726ea7deb966
Arley <abenagil456@gmail.com>
2025-06-12 23:03:12 -0500

    Upadate html, css, script

--- js/script.js
+++ js/script.js
@@ -5,15 +5,27 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   const links = document.querySelectorAll('.menu-header a')
+  document.getElementById('feature-scroll').addEventListener('click', () => {
+    document.getElementById('feature').scrollIntoView({ behavior: 'smooth' });
+  });
+
+  document.getElementById('team-scroll').addEventListener('click', () => {
+    document.getElementById('teams').scrollIntoView({ behavior: 'smooth' });
+  });
+
+  document.getElementById('sign-scroll').addEventListener('click', () => {
+    document.getElementById('sign').scrollIntoView({ behavior: 'smooth' });
+  });
+
   if (links.length > 0) {
     links[0].textContent = 'Features'
     links[0].href = '#feature'
 
     links[1].textContent = 'Team'
-    links[1].href = '#'
+    links[1].href = '#teams'
 
     links[2].textContent = 'Sign In'
-    links[2].href = '#'
+    links[2].href = '#sign'
   }
 
   const imagen = document.querySelector('#content img');
@@ -48,7 +60,7 @@ document.addEventListener('DOMContentLoaded', () => {
   function asignarValoresElements(elements, texts) {
     elements.forEach((element, index) => {
       if (index < texts.length) {
-        element.innerHTML = texts[index];
+        element.textContent = texts[index];
       }
     })
   }
@@ -96,5 +108,107 @@ document.addEventListener('DOMContentLoaded', () => {
 
   teamBoton.appendChild(teamIcon)
 
+  let experiences = document.querySelectorAll(".experiences p");
+  let nameExperience = document.querySelectorAll(".experiences h5");
+  let descriptionExperience = document.querySelectorAll(".experiences span");
+  
+  let paragraphExperiences = [
+    "Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.",
+    "Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine.",
+    " Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has become a well-oiled collaboration machine."
+  ]
+
+  let titleExperiences = [
+    "Satish Patel",
+    "Bruce McKenzie",
+    "Iva Boyd"
+  ]
+
+  let descriptionFounder = [
+    "Founder & CEO, Huddle",
+    "Founder & CEO, Huddle",
+    "Founder & CEO, Huddle"
+  ]
+
+  asignarValoresElements(experiences, paragraphExperiences)
+  asignarValoresElements(nameExperience, titleExperiences)
+  asignarValoresElements(descriptionExperience, descriptionFounder)
+
+  let iconFounder = document.querySelectorAll(".experiences img");
+  let iconSources = [
+    "images/profile-1.jpg",
+    "images/profile-2.jpg",
+    "images/profile-3.jpg"
+  ]
+
+  iconFounder.forEach((icons, index) => {
+    if (index <= iconSources.length) {
+      icons.src = iconSources[index]
+    }
+  })
+  
+  const formTitle = document.querySelector('.get-started h3');
+  const formDescription = document.querySelector('.get-started p');
+  const emailLabel = document.getElementById('email-label');
+  const emailInput = document.getElementById('email');
+  const formButton = document.getElementById('form-button');
+
+  formTitle.textContent = "Get early access today";
+  formDescription.textContent = "It only takes a minute to sign up and our free starter tier is extremely generous. If you have any questions, our support team would be happy to help you."
+  formButton.textContent = "Get Started For Free";
+
+  emailInput.setAttribute('name', 'email');
+  emailInput.setAttribute('placeholder', 'example@fylo.com');
+  emailInput.setAttribute('autocomplete', 'email');
+  emailInput.setAttribute('required', '');
+  emailLabel.setAttribute('for', 'email')
+
+  formButton.setAttribute('type', 'submit');
+  formButton.setAttribute('name', 'email');
+
+
+  let logoFooter = document.querySelectorAll('.footer img')
+  let listOne = document.querySelectorAll('.list-footer img')
+  let listOneLi = document.querySelectorAll('.list-footer p')
+
+  
+  function asignarIcon(imagenes, icon) {
+    imagenes.forEach((img, index) => {
+      if (index < icon.length) {
+        img.src = icon[index];
+      }
+    })
+  }
+
+  let logoFooterSrc = [
+    "images/logo.svg",
+  ]
+
+  let iconFooterSrc = [
+    "images/icon-location.svg",
+    "images/icon-phone.svg",
+    "images/icon-email.svg",
+    "images/facebook-brands.svg",
+    "images/square-twitter-brands.svg",
+    "images/instagram-brands.svg"
+  ] 
+
+
+  let listFooterOne = [
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+    "+1-543-123-4567",
+    "example@fylo.com",
+    "About Us",
+    "Jobs",
+    "Press",
+    "Blog",
+    "Contact Us",
+    "Terms",
+    "Privacy"
+  ]
+
+  asignarIcon(logoFooter, logoFooterSrc);
+  asignarIcon(listOne, iconFooterSrc);
+  asignarValoresElements(listOneLi, listFooterOne);
 
 });
